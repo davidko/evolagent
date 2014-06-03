325ecf6c34dc8929985e8b7dfaaaf0ae582c9fcf
David Ko <david.dk.ko@gmail.com>
2014-06-03 08:20:29 -0700

    Updated statistics w/ Harris's equation

--- test07_statistics/test.py
+++ test07_statistics/test.py
@@ -4,28 +4,53 @@ import numpy
 import pylab
 import random
 
-def test2():
-    N = 200
-    m = 5
-    iterations = 50000
+N = 200
+m = 10
+iterations = 50000
+
+def test3(i, N , m):
     bins = [0 for _ in range(N)]
     for _ in range(iterations):
-        choices = []
-        for _ in range(m):
-            choices.append(random.randint(0, N-1))
+        i = random.randint(1, N)
+        ranks = [x for x in range(0,N)]
+        rest = random.sample(ranks, m-1)
+        for _ in range(m - i):
+            #rand = random.randint(0,
+            rest.append(random.randint(0, N-1))
+    #pylab.plot(map(lambda x: float(x)/iterations, bins))
+
+def test2():
+    bins = [0 for _ in range(N)]
+    for _ in range(iterations):
+        ranks = [x for x in range(0, N)]
+        choices = random.sample(ranks, m)
         bins[min(choices)]+=1
     pylab.plot(map(lambda x: float(x)/iterations, bins))
     #pylab.show()
 
+def choose(n, k):
+    if k == 0:
+        return 1
+    if n == 0:
+        return 0
+    return choose(n-1, k-1) * n // k
+
+def f2(i, N, m):
+    return float(choose(N-i-1, m-1)) / choose(N, m)
+
 def f(i, N, m):
     numerator = 1
-    for j in range (1, m):
+    denominator = 1
+    k_fac = 1
+    for j in range (0, m-1):
         numerator *= (N-i-j)
-    return float(numerator)/(N**(m))
+        denominator *= (N-j)
+        k_fac *= j+1
+    denominator /= k_fac
+    return float(numerator)/denominator
 
 def main():
-    N = 200
-    m = 5
+    """
     data = [ f(x, N, m) for x in range(N) ] 
     print(data)
     mysum = numpy.sum(data)
@@ -33,6 +58,10 @@ def main():
     normalized_data = map(lambda x: x/mysum, data)
     print normalized_data
     pylab.plot(normalized_data)
+    """
+    data2 = [f2(x, N, m) for x in range(N) ]
+    print data2
+    pylab.plot(data2)
     pylab.show()
 
 if __name__ == '__main__':
